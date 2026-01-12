e19e853666f22ef2456917f1c4cce9b9810d5eb7
Nav Sharma <navdhruv12@gmail.com>
2026-01-12 16:47:20 +0530

    1s Code

---
+++ index.js
@@ -0,0 +1,12 @@
+// Reverse Words in a Sentence
+// Input: "Java is fun" → Output: "fun is Java"
+
+let s = "Java is fun";
+let result = s.split(" ").reverse().join(" ");
+console.log(result);
+
+// Check for Rotation
+// Given two strings, check if one is a rotation of another.
+// Input: "ABCD", "CDAB" → Output: true
+
+
